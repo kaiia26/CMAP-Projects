6b4b377c65ddff58e1ad20959db7c2bd4ac82a23
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-10-19 19:56:26 -0400

    Making a checkmark with asterisks

---
+++ Chapter 2/Checkmark.c
@@ -0,0 +1,19 @@
+/*Write a program that uses printf to display a checkmark on the screen */
+
+
+#include <stdio.h>
+
+int main() 
+{   
+    printf("         *");
+    printf("\n");
+    printf("        *");
+    printf("\n");
+    printf("       *");
+    printf("\n");
+    printf("*     *");
+    printf("\n""  *  *");
+    printf("\n""    *");
+
+    return 0;
+}
