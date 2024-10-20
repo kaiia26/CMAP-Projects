d16b9cb8ef618681bbef7e0800dca5d06a5b2f4f
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-10-19 21:13:08 -0400

    Computing volume of a sphere with a radius of 10

---
+++ Chapter 2/2. Computing the volume of a sphere.c
@@ -0,0 +1,18 @@
+/* Write a program that computes the volume of a sphere with a 10-meter radius, using the formula V = 4/3π r^3. Write the fraction
+4/3 as 4.0f/3.0f.(Try writing it as 4/3. What happens?)(Hint: C doesn't have an exponentiation operator, so you'll need to
+multiply r by itself three times to compute r^3.)*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+int volume, radius ;
+
+radius = 10;
+volume = printf("Volume: %f%f%d%i%i%i", 4.0f / 3.0f * 3.14159f * radius * radius * radius) ;
+
+return 0;
+    
+}
+
