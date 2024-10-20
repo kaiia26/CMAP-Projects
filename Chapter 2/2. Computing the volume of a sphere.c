093a04039f2dac745ee9d153b433a7349316282c
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-10-19 21:48:16 -0400

    Prompts user to enter a desired radius

--- Chapter 2/2. Computing the volume of a sphere.c
+++ Chapter 2/2. Computing the volume of a sphere.c
@@ -2,15 +2,18 @@
 4/3 as 4.0f/3.0f.(Try writing it as 4/3. What happens?)(Hint: C doesn't have an exponentiation operator, so you'll need to
 multiply r by itself three times to compute r^3.)*/
 
+/* Modify the program so it prompts the user to enter the radius of a sphere*/
+
 #include <stdio.h>
 
 int main(void)
 {
 
-int volume, radius ;
+int volume, radius;
 
-radius = 10;
-volume = printf("Volume: %f%f%d%i%i%i", 4.0f / 3.0f * 3.14159f * radius * radius * radius) ;
+printf("Print out the desired radius: ");
+scanf("%i", &radius);
+volume = printf("Volume: %f%f%d%i%i%i", 4.0f / 3.0f * 3.14159f * radius * radius * radius);
 
 return 0;
     
