566c626dae011caf863f947b008271ce704147c6
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-10-21 20:02:33 -0400

    Compute polynomial from user input

---
+++ Chapter 2/Polynomial.c
@@ -0,0 +1,22 @@
+/* Write a program that asks the user to enter a value for x and then displays the value of the following polynomial
+
+    3x^5 + 2x^4 -5x^3 -x^2 +7x-6
+*/
+
+#include <stdio.h>
+
+int main(void) 
+{
+    int x, polynomial;
+    printf("Enter a value for x: ");
+    scanf("%i", &x);
+
+    polynomial = (3 * x * x * x * x * x) + (2 * x * x * x * x) - (5 * x * x * x) - (x * x) + (7 * x * x * x * x * x * x);
+    printf("Answer: %i", polynomial);
+
+
+    return 0;
+
+
+
+}
