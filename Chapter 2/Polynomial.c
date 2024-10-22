004215e5b058b3e2be0e23b07758887a29de236e
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-10-21 21:34:02 -0400

    Horner's Rule version of Polynomial.c

--- Chapter 2/Polynomial.c
+++ Chapter 2/Polynomial.c
@@ -1,19 +1,27 @@
-/* Write a program that asks the user to enter a value for x and then displays the value of the following polynomial
+/* Modify the program of Polynomial.c so that the polynomial is evaluated using the following formula:
+    
+    ((((3x + 2)x-5)x-1)x+7)x-6
 
-    3x^5 + 2x^4 -5x^3 -x^2 +7x-6
 */
 
 #include <stdio.h>
 
 int main(void) 
 {
-    int x, polynomial;
+    int x, p1, p2, p3, p4, p5, result;
     printf("Enter a value for x: ");
     scanf("%i", &x);
 
-    polynomial = (3 * x * x * x * x * x) + (2 * x * x * x * x) - (5 * x * x * x) - (x * x) + (7 * x * x * x * x * x * x);
-    printf("Answer: %i", polynomial);
+ /*p starts for parts of the problem, we break it down into smaller pieces; there are 5 parts in total*/
+    p1 = 3 * x + 2;
+    p2 = p1 * x - 5;
+    p3 = p2 * x - 1;
+    p4 = p3 * x + 7;
+    p5 = p4 * x - 6;  /*Since p5 is the accumulation of previous calculations, it is the answer*/
+    
+    result = p5;     /*Relabeled as result for proper comprehension*/
 
+    printf("Answer: %i", result);
 
     return 0;
 
