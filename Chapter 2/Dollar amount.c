2dc7c488cb9e578159b8c57a6544ea5687ff130d
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-10-22 21:05:51 -0400

    Put newlines for readability

--- Chapter 2/Dollar amount.c
+++ Chapter 2/Dollar amount.c
@@ -10,26 +10,26 @@ int main(void)
 {
 
 int amount, amountNeeded20, amountNeeded10, amountNeeded5, amountNeeded1, remainingAmount20, remainingAmount10,
-remainingAmount5, remainingAmount1 amountLeft;
+remainingAmount5, remainingAmount1;
 
 printf("Enter a dollar amount: ");
 scanf("%i", &amount);
 
 amountNeeded20 = amount / 20;  // For instance 93 / 20 = 4.65 or 4 // or $10 left because 20 x 4 = 80, and 90 - 80 is $10
 remainingAmount20 = amount - (amountNeeded20 * 20); // gives $13 left
-printf("$20 bills: %i", amountNeeded20);  // prints out 4 $20 bills
+printf("$20 bills: %i\n", amountNeeded20);  // prints out 4 $20 bills
 
 amountNeeded10 = remainingAmount20 / 10;  // Gives 13 / 10, which is 1.3, rounds down to 1
 remainingAmount10 = remainingAmount20 - (amountNeeded10 * 10); // gives $3 left
-printf("$10 bills: %i", amountNeeded10); // prints out 1 $10 bill
+printf("$10 bills: %i\n", amountNeeded10); // prints out 1 $10 bill
 
 amountNeeded5 = remainingAmount10 / 5; // Gives 3 / 5 which is 0.6, rounds down to 0
 remainingAmount5 = remainingAmount10 - (amountNeeded5 * 5); // gives $3 left
-printf("$5 bills: %i", amountNeeded5); //prints out 0 $5 bills
+printf("$5 bills: %i\n", amountNeeded5); //prints out 0 $5 bills
 
 amountNeeded1 = remainingAmount5 / 1; // Gives 3 / 1 which is 3
 remainingAmount1 = remainingAmount5 - (amountNeeded1 * 1); // gives $0 left
-printf("$1 bills: %i", amountNeeded1); //prints out 3 $1 bills
+printf("$1 bills: %i\n", amountNeeded1); //prints out 3 $1 bills
 
 
     return 0;
