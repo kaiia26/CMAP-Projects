51603b8adb2f2b3f3ff3798866e47197aeb5b937
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-10-24 11:26:00 -0400

    Updated methods to calculate balance

--- Chapter 2/Loan balance.c
+++ Chapter 2/Loan balance.c
@@ -8,15 +8,16 @@
 int main(void)
 {
 
-float loan, balance, increaseBalance, decreaseBalance, decreaseBalance1, decreaseBalance2, decreaseBalance3, 
-interestRate, monthlyInterestRate, monthlyPayment, firstPayment, secondPayment, thirdPayment;  
+float loan, balance, increaseBalance, upd_increaseBalance2, upd_increaseBalance3,
+decreaseBalance, upd_decreaseBalance2, upd_decreaseBalance3, updatedBalance, updatedBalance2, updatedBalance3, interestRate,
+monthlyInterestRate, monthlyPayment, firstPayment, secondPayment, thirdPayment;  
 
 printf("Enter the amount of loan: ");
 scanf("%.2f", &loan );
 
 printf("Enter interest rate: ");
 scanf("%f", &interestRate);
-printf("\n");
+printf("\n");                                     //monthly payment and interest rate are constant throughout the program
 
 printf("Enter monthly payment: ");
 scanf("%.2f", &monthlyPayment);
@@ -25,13 +26,26 @@ printf("\n");
 monthlyInterestRate = interestRate / 12.0;      // interest rate is divided by 12 months to calculate monthly
 
 balance = loan;    // balance is the loan that the user has
-decreaseBalance = balance - monthlyPayment;   // balance is decreased by the user paying monthly payments
-increaseBalance = balance * monthlyInterestRate;     // balance is increased by the monthly interest rate   
+decreaseBalance = balance - monthlyPayment;   // balance is decreased each month by the user paying monthly payments
+increaseBalance = balance * monthlyInterestRate;     // balance is increased each month by the monthly interest rate   
 
-
-
+updatedBalance = balance - decreaseBalance;
+updatedBalance = balance + increaseBalance;
+firstPayment = updatedBalance;
 printf("Balance remaining after the first payment: %.2f\n", firstPayment);
+
+upd_decreaseBalance2 = firstPayment - monthlyPayment; //recalculated decrease balance to fit updated balance for second payment
+upd_increaseBalance2 = firstPayment * monthlyInterestRate;  //recalculated increase balance to fit updated balance for second payment
+updatedBalance2 = firstPayment - upd_decreaseBalance2; 
+updatedBalance2 = firstPayment + upd_increaseBalance2;
+secondPayment = updatedBalance2;
 printf("Balance remaining after the second payment: %.2f\n", secondPayment);
+
+upd_decreaseBalance3 = secondPayment - monthlyPayment;
+upd_increaseBalance3 = secondPayment * monthlyInterestRate;
+updatedBalance3 = secondPayment - upd_decreaseBalance3;
+updatedBalance3 = secondPayment + upd_increaseBalance3;
+thirdPayment = updatedBalance3;
 printf("Balance remaining after the third payment: %.2f\n", thirdPayment);
 
 
