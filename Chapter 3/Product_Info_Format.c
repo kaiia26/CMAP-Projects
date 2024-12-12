a767f678ab4a352143f099546e5bb64adcca37f0
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-12-11 21:48:27 -0500

    Fixed formatting for description

--- Chapter 3/Product_Info_Format.c
+++ Chapter 3/Product_Info_Format.c
@@ -11,22 +11,22 @@ int item_number, purchase_date, month, day, year;
 float unit_price;
 
 printf("Enter item number: ");
-scanf("%d", item_number);
+scanf("%d", &item_number);
 
 printf("Enter item price: ");
-scanf("%.1f", unit_price);
+scanf("%f", &unit_price);
 
 printf("Enter purchase date (mm/dd/yyyy): ");
 scanf("%d/%d/%d", &month, &day, &year );
 
 
-printf("Item\t\t Unit\t\t Purchase");         // tabs for formatting
-printf("\t\tPrice\t\t Date");               // tabs for formatting
-printf("%-10d", item_number);                //left-justifies item_number and adds spaces between item and unit price columns
-printf("$%2d");                            // right-justifies and adds space between $ symbol and the unit_price
-printf("%4.2f", unit_price);              // specifies float format specifier for unit price to be counted as two decimals
-printf("%-5d");                            // left-justifies to add space between unit_price and purchase_date columns
-printf("%0d/%0d/%d", month, day, year);       //input is padded by 0 in case there is a single value 
+printf("\n");
+printf("Item\t\tUnit\t\tPurchase\n\t\t\tPrice\t\tDate");
+printf("%-10d", item_number);                
+printf("$%2d");                           
+printf("%4.2f", unit_price);              
+printf("%-5d");                            
+printf("%0d/%0d/%d", month, day, year);
 
 
 return 0;
