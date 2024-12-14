79c39a04ee39113332b9c265126bb03c86da393e
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-12-14 12:32:58 -0500

    Formatting for the first row

--- Chapter 3/MagicSquare.c
+++ Chapter 3/MagicSquare.c
@@ -14,6 +14,9 @@ printf("Enter the numbers from 1 to 16 in any order: ");
 scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", 
 &num1, &num2, &num3, &num4, &num5, &num6, &num7, &num8, &num9, &num10, &num11, &num12, &num13, &num14, &num15, &num16);
 
+printf("\n");
+printf("%d\t%d\t%d\t%d", num1, num2, num3, num4);
+printf("\n")
 
 
 
