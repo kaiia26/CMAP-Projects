f5fe1f61ecc6365681c650224dd53244e2d54116
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-12-14 12:53:47 -0500

    Created variables for calculations

--- Chapter 3/MagicSquare.c
+++ Chapter 3/MagicSquare.c
@@ -8,6 +8,7 @@ int main(void)
 {
 
 int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, num11, num12, num13, num14, num15, num16;
+int row_1, row_2, row_3, row_4, column_1, column_2, column_3, column_4, diagonal_1, diagonal_2;
 
 
 printf("Enter the numbers from 1 to 16 in any order: ");
@@ -16,8 +17,17 @@ scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d",
 
 printf("\n");
 printf("%d\t%d\t%d\t%d", num1, num2, num3, num4);
-printf("\n")
+printf("\n");
+printf("%d\t%d\t%d\t%d", num5, num6, num7, num8);
+printf("\n");
+printf("%d\t%d\t%d\t%d", num9, num10, num11, num12);
+printf("\n");
+printf("%d\t%d\t%d\t%d", num13, num14, num15, num16);
+
+printf("Row sums: %d %d %d %d", row_1, row_2, row_3, row_4);
 
 
+
+return 0;
 
 }
