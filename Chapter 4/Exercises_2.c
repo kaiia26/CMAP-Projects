84e870ef41591e8bdbdf93030928993f49f6de46
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-12-19 10:51:49 -0500

    Solved 10a with steps included for clarity

---
+++ Chapter 4/Exercises_2.c
@@ -0,0 +1,13 @@
+// 10. Show the output produced by each of the following program fragments. Assume that i and j are int variables.
+
+// (a) i = 6;
+// j = i += i
+// printf("%d %d", i, j);
+
+// i = 6
+// i = i + 6; i = 6 + 6; i = 12
+// j = 12
+// Answer: i and j are both 12
+
+
+
