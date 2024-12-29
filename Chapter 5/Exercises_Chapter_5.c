670626d380f6b11af88e31cd7fcad258c0892232
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2024-12-29 16:31:17 -0500

    Cleaned up code

--- Chapter 5/Exercises_Chapter_5.c
+++ Chapter 5/Exercises_Chapter_5.c
@@ -195,6 +195,30 @@ int main(void)
 // 8. The following if statement is unnecessarily complicated. Simplify it as much as possible
 // (Hint: The entire statement can be replaced by a single assignment)
 
+/*
+#include <stdio.h>
+
+int main(void)
+
+int teenager, age;
+{
+    if (age >= 13) {
+        if (age <= 19)
+            teenager = true;
+        else 
+            teenager = false;
+    else if (age < 13)
+        teenager = false;
+
+
+
+
+    return 0;
+}
+
+*/
+
+// Answer: (age >= 13) ? teenager = true : (age <= 19) ? teenager = true : teenager = false );
 
 
 
