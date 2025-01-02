ef858722104a2ae544d54eced68332bdb423ef6a
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2025-01-02 18:33:18 -0500

    Adjusted hours according to subtraction formula

---
+++ Chapter 5/Programming Projects/Display_Time.c
@@ -0,0 +1,26 @@
+/* Write a program that asks the user for a 24-hour time, then displays the time in 12-hour form */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int hour, minutes, reg_hour;
+
+    printf("Enter a 24-hour time: ");
+    scanf("%2d:%2d", &hour, &minutes);
+
+    reg_hour = hour - 12;
+
+    if (hour >= 11 && hour <= 24) {
+        printf("Equivalent 12-hour time: %2d:%2d AM", reg_hour, minutes);
+    } else if (hour >= 13 && hour <= 23) {
+        printf("Equivalent 12-hour time: %2d:%2d PM", reg_hour, minutes);
+    } else if (reg_hour == 12) {
+        printf("Equivalent 12-hour time: %2d:%2d PM", reg_hour, minutes);
+    } else if (hour == 0) {
+        reg_hour = 12;
+        printf("Equivalent 12-hour time: %2d:%2d AM", reg_hour, minutes);
+    }
+
+    return 0;
+}
