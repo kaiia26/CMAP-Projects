d67228ed79f3540adb5dd056b1264476a1cda883
Kaia Perez Hvala <kaiavictoriap@gmail.com>
2025-01-02 18:50:09 -0500

    Adjusted not equivalent to statement with 12:00

--- Chapter 5/Programming Projects/Display_Time.c
+++ Chapter 5/Programming Projects/Display_Time.c
@@ -11,15 +11,16 @@ int main(void)
 
     reg_hour = hour - 12;
 
-    if (hour >= 11 && hour <= 24) {
-        printf("Equivalent 12-hour time: %2d:%2d AM", reg_hour, minutes);
+
+    if (hour >= 11 && hour != 12 && hour <= 24) {
+        printf("Equivalent 12-hour time: %2d:%02d AM", reg_hour, minutes);
     } else if (hour >= 13 && hour <= 23) {
-        printf("Equivalent 12-hour time: %2d:%2d PM", reg_hour, minutes);
-    } else if (reg_hour == 12) {
-        printf("Equivalent 12-hour time: %2d:%2d PM", reg_hour, minutes);
+        printf("Equivalent 12-hour time: %2d:%02d PM", reg_hour, minutes);
+    } else if (hour == 12) {
+        printf("Equivalent 12-hour time: %2d:%02d PM", hour, minutes);
     } else if (hour == 0) {
         reg_hour = 12;
-        printf("Equivalent 12-hour time: %2d:%2d AM", reg_hour, minutes);
+        printf("Equivalent 12-hour time: %2d:%02d AM", reg_hour, minutes);
     }
 
     return 0;
